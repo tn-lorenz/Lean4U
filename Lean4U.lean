29810880687871f33abea6c3ad8157c547211682
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-07-10 10:37:51 +0200

    I might be retarded

--- Lean4U.lean
+++ Lean4U.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Lean4U` library.
 -- Import modules here that should be built as part of the library.
 import Lean4U.Basic
+import Lean4U.year1.semester1.logic_and_set_theory.easy_intro
---
+++ Lean4U/year1/semester1/logic_and_set_theory/easy_intro.lean
@@ -0,0 +1,5 @@
+namespace Lean4U.year1.semester1.logic_and_set_theory.easy_intro
+
+def hello2 := "world"
+
+end Lean4U.year1.semester1.logic_and_set_theory.easy_intro
